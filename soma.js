6a20fc4067cb347d054f77345e6f8e6929443c47
Augusto de Souza Silva <augusto.silva50@fatec.sp.gov.br>
2023-11-17 22:17:29 -0300

    Update soma.js

--- soma.js
+++ soma.js
@@ -1,22 +1,15 @@
-function soma_multiplos(n) {
-    // Verifica se o número é positivo
-    if (n < 1) {
-      throw new Error("O número deve ser positivo");
+function somatorioDivisiveis(num) {
+  let soma = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      soma += i;
     }
-  
-    // Inicializa a variável de soma
-    let soma = 0;
-  
-    // Itera sobre todos os números inteiros de 1 até n
-    for (let i = 1; i < n; i++) {
-      // Verifica se o número é divisível por 3 ou 5
-      if (i % 3 === 0 || i % 5 === 0) {
-        // Adiciona o número à soma
-        soma += i;
-      }
-    }
-  
-    // Retorna a soma
-    return soma;
   }
-  
+
+  return soma;
+}
+
+// Exemplos de uso:
+console.log(somatorioDivisiveis(10)); // Saída: 23
+console.log(somatorioDivisiveis(11)); // Saída: 33
